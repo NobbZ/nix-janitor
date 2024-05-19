6f92f96f6893b00def1b1b18d230076470637667
Norbert Melzer <timmelzer@gmail.com>
2024-05-19 14:27:57 +0200

    chore: format nix

--- flake.nix
+++ flake.nix
@@ -11,46 +11,54 @@
     cargo2nix.inputs.rust-overlay.follows = "rust-overlay";
   };
 
-  outputs = {
-    parts, ...
-  }@inputs: parts.lib.mkFlake {inherit inputs;} {
-    systems = ["x86_64-linux" "aarch64-linux"];
+  outputs = {parts, ...} @ inputs:
+    parts.lib.mkFlake {inherit inputs;} {
+      systems = ["x86_64-linux" "aarch64-linux"];
 
-    perSystem = {self', pkgs, system, ...}: let
-    pkgsWithOverlays = (inputs.nixpkgs.legacyPackages.${system}.extend inputs.rust-overlay.overlays.default).extend inputs.cargo2nix.overlays.default;
-    rustVersion = "1.78.0";
-    rust = pkgs.rust-bin.stable.${rustVersion}.default;
-    in {
-      _module.args.pkgs = pkgsWithOverlays;
+      perSystem = {
+        self',
+        pkgs,
+        system,
+        ...
+      }: let
+        pkgsWithOverlays = (inputs.nixpkgs.legacyPackages.${system}.extend inputs.rust-overlay.overlays.default).extend inputs.cargo2nix.overlays.default;
+        rustVersion = "1.78.0";
+        rust = pkgs.rust-bin.stable.${rustVersion}.default.override {
+          # extensions = [ "rust-src" ];
+        };
+      in {
+        _module.args.pkgs = pkgsWithOverlays;
 
-      formatter = pkgs.alejandra;
+        formatter = pkgs.alejandra;
 
-      legacyPackages.janitorPkgsBuilder = pkgs.rustBuilder.makePackageSet {
-        inherit rustVersion;
-        # extraRustComponents = [ "rust-src" ];
-        packageFun = import "${inputs.self}/Cargo.nix";
-      };
+        legacyPackages.janitorPkgsBuilder = pkgs.rustBuilder.makePackageSet {
+          inherit rustVersion;
+          # extraRustComponents = [ "rust-src" ];
+          packageFun = import "${inputs.self}/Cargo.nix";
+        };
 
-      legacyPackages.helpers.testrunner = pkgs.writeShellScriptBin "testrunner" ''
-        ${pkgs.inotify-tools}/bin/inotifywait -m -r -e close_write,moved_to --format '%w%f' src | \
-          while read dir action file; do
-            cargo nextest run
-            cargo test --doc
-            cargo doc
-          done
-      '';
+        legacyPackages.helpers.testrunner = pkgs.writeShellScriptBin "testrunner" ''
+          ${pkgs.inotify-tools}/bin/inotifywait -m -r -e close_write,moved_to --format '%w%f' src | \
+            while read dir action file; do
+              cargo nextest run
+              cargo test --doc
+              cargo doc
+            done
+        '';
 
-      packages.janitor = (self'.legacyPackages.janitorPkgsBuilder.workspace.janitor {}).bin;
-      packages.default = self'.packages.janitor;
+        packages.janitor = (self'.legacyPackages.janitorPkgsBuilder.workspace.janitor {}).bin;
+        packages.default = self'.packages.janitor;
 
-      devShells.default = pkgs.mkShell {
-        packages = builtins.attrValues {
-          inherit (pkgs) cargo-nextest cargo-audit cargo-deny cargo-tarpaulin rust-analyzer;
-          inherit (pkgs) nil;
-          inherit (self'.legacyPackages.helpers) testrunner;
-          inherit rust;
+        devShells.default = pkgs.mkShell {
+          packages = builtins.attrValues {
+            inherit (pkgs) cargo-nextest cargo-audit cargo-deny cargo-tarpaulin rust-analyzer;
+            inherit (pkgs) nil;
+            inherit (self'.legacyPackages.helpers) testrunner;
+            inherit rust;
+          };
+
+          # env.RUST_SRC_PATH = "${rust}/lib/rustlib/src/rust/library";
         };
       };
     };
-  };
 }
