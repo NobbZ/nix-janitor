ae073a16aebccc6eda80fd67e5ac09170d6ba419
Norbert Melzer <timmelzer@gmail.com>
2024-05-21 10:49:43 +0200

    refactor: use user cache more thoroughly

--- src/profiles.rs
+++ src/profiles.rs
@@ -1,17 +1,36 @@
-use std::path::{Path, PathBuf};
+use std::{
+    fmt,
+    path::{Path, PathBuf},
+    sync::{Arc, Mutex},
+};
 
 use eyre::Result;
-use uzers::UsersCache;
+use uzers::{Users, UsersCache};
 
 use crate::user;
 
 /// Represents a Nix profile path.
 ///
 /// This wraps a [std::path::PathBuf] to provide a named type.
-#[derive(Debug)]
-pub struct Profile(PathBuf);
+pub struct Profile<U>(PathBuf, Arc<Mutex<U>>)
+where
+    U: Users;
 
-impl Profile {
+#[cfg(not(tarpaulin_include))]
+impl<U> fmt::Debug for Profile<U>
+where
+    U: Users,
+{
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        f.debug_tuple("Profile")
+            .field(&self.0)
+            .field(&format_args!("_"))
+            .finish()
+    }
+}
+
+#[cfg(not(tarpaulin_include))]
+impl Profile<UsersCache> {
     /// Creates a new Profile from the given path.
     ///
     /// # Arguments
@@ -26,7 +45,7 @@ impl Profile {
     /// let profile = Profile::new("/foo/bar");
     /// ```
     pub fn new<P: Into<PathBuf>>(path: P) -> Self {
-        Self(path.into())
+        Self(path.into(), Arc::new(Mutex::new(UsersCache::new())))
     }
 
     /// Returns all default profile paths for the current user.
@@ -41,39 +60,54 @@ impl Profile {
     /// let profiles = Profile::all();
     /// ```
     pub fn all() -> Vec<Self> {
-        let mut users = UsersCache::new();
+        let users = Arc::new(Mutex::new(UsersCache::new()));
+
+        Self::all_with_users(users)
+    }
+}
+
+impl<U> Profile<U>
+where
+    U: Users,
+{
+    fn all_with_users(users: Arc<Mutex<U>>) -> Vec<Self> {
+        let ctx = |s: &str| context(s, users.clone());
 
         let mut paths = vec![
             "/nix/var/nix/profiles/per-user/$USER/profile",
             "/home/$USER/.local/state/nix/profiles/home-manager",
         ];
 
-        if user::is_root(&mut users) {
+        if user::is_root(users.clone()) {
             paths.push("/nix/var/nix/profiles/system");
         }
 
         paths
             .iter()
-            .map(|p| -> Result<_> { Ok(shellexpand::env_with_context(p, context).unwrap()) })
+            .map(|p| -> Result<_> { Ok(shellexpand::env_with_context(p, ctx).unwrap()) })
             .map(|p| -> Result<_> { Ok(PathBuf::from(p?.to_string())) })
             .filter_map(|pr| pr.ok())
-            .filter(|p| p.exists())
-            .map(Self::new)
+            // .filter(|p| p.exists())
+            .map(|p| Self(p, users.clone()))
             .collect::<Vec<_>>()
     }
 }
 
-impl AsRef<Path> for Profile {
+impl<U> AsRef<Path> for Profile<U>
+where
+    U: Users,
+{
     fn as_ref(&self) -> &Path {
         &self.0
     }
 }
 
-fn context(s: &str) -> Result<Option<String>> {
-    let mut users = UsersCache::new();
-
+fn context<U>(s: &str, users: Arc<Mutex<U>>) -> Result<Option<String>>
+where
+    U: Users,
+{
     match s {
-        "USER" => Ok(user::get_real_username(&mut users)),
+        "USER" => Ok(user::get_real_username(users.clone())),
         v => Err(eyre::eyre!("unknown variable: {v}")),
     }
 }
@@ -83,15 +117,43 @@ mod test {
     use super::*;
 
     use proptest::prelude::*;
+    use temp_env::with_vars;
+    use uzers::mock::MockUsers;
 
     proptest! {
         #[test]
         fn new(path in "(/[a-z]+)+") {
             let path = PathBuf::from(&path);
             let profile = Profile::new(&path);
-            assert_eq!(profile.0, path);
+            prop_assert_eq!(profile.0, path);
+        }
+
+        #[test]
+        fn test_being_regular_user(uid in 1u32..20_000, username in "[a-z]+") {
+            let users = Arc::new(Mutex::new(MockUsers::with_current_uid(uid)));
+
+            let profiles = with_vars([("USER", Some(&username))], || Profile::all_with_users(users.clone()));
+
+            let paths = profiles.iter().map(|p| PathBuf::from(p.as_ref())).collect::<Vec<_>>();
+
+            let user_profile = PathBuf::from(format!("/nix/var/nix/profiles/per-user/{}/profile", username));
+            let home_profile = PathBuf::from(format!("/home/{}/.local/state/nix/profiles/home-manager", username));
+
+            prop_assert_eq!(paths, vec![user_profile, home_profile]);
+        }
+
+        #[test]
+        fn test_being_sudo_user(username in "[a-z]+") {
+            let users = Arc::new(Mutex::new(MockUsers::with_current_uid(0)));
+
+            let profiles = with_vars([("SUDO_USER", Some(&username)), ("USER", Some(&"root".to_owned()))], || Profile::all_with_users(users.clone()));
+
+            let paths = profiles.iter().map(|p| PathBuf::from(p.as_ref())).collect::<Vec<_>>();
+
+            let user_profile = PathBuf::from(format!("/nix/var/nix/profiles/per-user/{}/profile", username));
+            let home_profile = PathBuf::from(format!("/home/{}/.local/state/nix/profiles/home-manager", username));
+
+            prop_assert_eq!(paths, vec![user_profile, home_profile, PathBuf::from("/nix/var/nix/profiles/system")]);
         }
     }
-
-    // TODO: provide some tests for Profile::all()
 }
--- src/user.rs
+++ src/user.rs
@@ -1,15 +1,16 @@
 use std::env;
+use std::sync::{Arc, Mutex};
 
 use uzers::Users;
 
-pub fn is_root<U>(users: &mut U) -> bool
+pub fn is_root<U>(users: Arc<Mutex<U>>) -> bool
 where
     U: Users,
 {
-    users.get_current_uid() == 0
+    users.lock().unwrap().get_current_uid() == 0
 }
 
-pub fn get_real_username<U>(users: &mut U) -> Option<String>
+pub fn get_real_username<U>(users: Arc<Mutex<U>>) -> Option<String>
 where
     U: Users,
 {
@@ -33,25 +34,25 @@ mod tests {
     proptest! {
         #[test]
         fn test_is_root(uid in 1u32..20_000) {
-            let mut users = MockUsers::with_current_uid(uid);
+            let users = Arc::new(Mutex::new(MockUsers::with_current_uid(uid)));
 
-            prop_assert!(!is_root(&mut users));
+            prop_assert!(!is_root(users));
         }
 
         #[test]
         fn test_get_real_username(uid in 1u32..20_000, username in "[a-z]+") {
-            let mut users = MockUsers::with_current_uid(uid);
+            let users = Arc::new(Mutex::new(MockUsers::with_current_uid(uid)));
 
-            let real_username = with_vars([("USER", Some(&username))], || get_real_username(&mut users));
+            let real_username = with_vars([("USER", Some(&username))], || get_real_username(users));
 
             prop_assert_eq!(real_username, Some(username.to_string()));
         }
 
         #[test]
         fn test_get_real_username_root(username in "[a-z]+") {
-            let mut users = MockUsers::with_current_uid(0);
+            let users = Arc::new(Mutex::new(MockUsers::with_current_uid(0)));
 
-            let real_username = with_vars([("SUDO_USER", Some(&username)), ("USER", Some(&"root".to_owned()))], || get_real_username(&mut users));
+            let real_username = with_vars([("SUDO_USER", Some(&username)), ("USER", Some(&"root".to_owned()))], || get_real_username(users));
 
             assert_eq!(real_username, Some(username.to_string()));
         }
@@ -59,8 +60,8 @@ mod tests {
 
     #[test]
     fn test_is_root_root() {
-        let mut users = MockUsers::with_current_uid(0);
+        let users = Arc::new(Mutex::new(MockUsers::with_current_uid(0)));
 
-        assert!(is_root(&mut users));
+        assert!(is_root(users));
     }
 }
